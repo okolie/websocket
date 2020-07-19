96fb0e66a8e89d143f3d425b321cbd8e4bf739e3
Okolie <okoliechigozie@yahoo.com>
2020-07-19 21:22:06 +0100

    setting up websocket

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require( "express" );
+const socket = require( "socket.io" );
 const app = express();
 const port = 4000;
+
+
+
+
 
 app.listen( port, () => console.log( `Server running on port ${ port }!` ) );
 
