12df0f39b0d71675202f54e76f1ac111921bcb50
Okolie <okoliechigozie@yahoo.com>
2020-07-21 11:03:08 +0100

    Updated server functionality

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express = require( "express" );
-const socket = require( "socket.io" );
-const app = express();
-const port = 4000;
+var express = require("express");
+var socket = require("socket.io");
+var app = express();
+var port = 4000;
 
+var server = app.listen(port, () =>
+	console.log(`Server running on port ${port}!`)
+);
 
+var io = socket(server);
 
+io.on("connection", function (socket) {
+	console.log(`socket is connected`);
+	socket.on("chat", function (clientData) {
+		io.sockets.emit("chat", clientData);
+	});
 
+	socket.on("typing", function (feedback) {
+		socket.broadcast.emit("typing", feedback);
+	});
+});
 
-app.listen( port, () => console.log( `Server running on port ${ port }!` ) );
-
-app.use( express.static( "public" ) );
+app.use(express.static("public"));
